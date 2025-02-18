9952f975c96fd6e7d57562087394582beb201446
karamathasan <94758689+karamathasan@users.noreply.github.com>
2025-02-18 13:46:17 -0500

    first commit

---
+++ client/main.py
@@ -0,0 +1,126 @@
+import asyncio
+import pygame
+import keyboard
+import websockets
+import json
+
+# pygame setup
+
+pygame.init()
+joystick = pygame.joystick.Joystick(0)
+# rightStick = pygame.joystick.Joystick(1)
+pygame.joystick.init()
+
+height = 300
+width = 400
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+running = True
+ip = "192.168.1.61"
+# ip = "192.168.1.77"
+
+# phone ip: 192.168.160.99
+
+def UI(left, right):
+    pygame.draw.line(screen,"red",pygame.Vector2(width/2 - 50,height/2), pygame.Vector2(width/2 - 50, left * 100 + height/2), 25)
+    pygame.draw.line(screen,"blue",pygame.Vector2(width/2 + 50,height/2), pygame.Vector2(width/2 + 50, right * 100 + height/2), 25)
+
+# async def quit(queue):
+#     await queue.put(json.dumps({"left":0,"right":0}))
+#     await queue.put("quit")  # Tell WebSocket client to close
+
+# async def drive_client(motion):
+#     async with websockets.connect(f"ws://{ip}:80/drive") as websocket:
+#         print("Connected to Drive server")
+#         while running:
+#             inputs = await motion.get()
+#             if inputs == "quit":
+#                 break
+            
+#             await websocket.send(inputs)
+
+# async def servo_client(servo):
+#     async with websockets.connect(f"ws://{ip}:80/servo") as websocket:
+#         print("Connected to Servo server")
+#         while running:
+#             inputs = await servo.get()
+#             if inputs == "quit":
+#                 break
+            
+#             await websocket.send(inputs)
+
+
+async def main():
+    # servoDown = False
+    # motionQueue = asyncio.Queue()
+    # servoQueue = asyncio.Queue()
+    # drive_task = asyncio.create_task(drive_client(motionQueue))
+    # servo_task = asyncio.create_task(servo_client(servoQueue))
+    
+    running = True
+    leftOld = joystick.get_axis(1)
+    rightOld = joystick.get_axis(3)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # await quit(motionQueue)
+                # drive_task.cancel()
+                # servo_task.cancel()
+                running = False
+                break
+
+            # elif event.type == pygame.JOYAXISMOTION:
+            #     leftDelta = joystick.get_axis(1) - leftOld
+            #     leftOld = joystick.get_axis(1)
+            #     rightDelta = joystick.get_axis(3) - rightOld
+            #     rightOld = joystick.get_axis(3)
+            #     if abs(leftDelta) > 0.005 or abs(rightDelta) > 0.005:
+            #         data = json.dumps(
+            #             {
+            #                 "left":(round(joystick.get_axis(1),4)),
+            #                 "right":(round(joystick.get_axis(3),4))
+            #             }
+            #         )
+            #         await motionQueue.put(data)
+
+            # elif event.type == pygame.JOYBUTTONDOWN: 
+            #     if event.button == 10:
+            #         data = json.dumps({"servo":0})
+            #         if servoDown == False:
+            #             data = json.dumps(
+            #                 {"servo":50}
+            #             )
+            #             servoDown = True
+            #         else:
+            #             data = json.dumps(
+            #                 {"servo":0}
+            #             )
+            #             servoDown = False
+            #         print(f"right bumper pressed: {servoDown}")
+            #         await servoQueue.put(data)
+            #         if servoDown == True:
+            #             await asyncio.sleep(0.3)
+            #             await servoQueue.put(json.dumps({"servo":30}))
+
+            #     if event.button == 9:
+            #         print("left shoulder pressed")
+
+
+            # elif event.type == pygame.JOYBUTTONUP:
+            #     if event.button == 10:
+            #         data = json.dumps(
+            #             {"servo":0}
+            #         )
+            #         await servoQueue.put(data)
+                
+        screen.fill((0, 0, 0))
+        UI(joystick.get_axis(1),joystick.get_axis(3))
+        pygame.display.flip()
+        
+        # await asyncio.sleep(0.01)
+    # await drive_task
+    # await servo_task
+
+# Run the event loop
+asyncio.run(main())
+pygame.quit()
---
+++ server/server.py
@@ -0,0 +1,75 @@
+from microdot import Microdot
+from microdot.websocket import with_websocket
+from microdot.cors import CORS
+from machine import Pin, PWM
+import ujson
+import asyncio 
+import network
+import env
+import time
+
+print("board start")
+LED = Pin("LED",Pin.OUT)
+LED.toggle()
+
+
+# motor 1 is LF, motor 2 is LB
+# motor 3 is RB, motor 4 is LF
+
+# Connect to Wi-Fi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(env.SSID, env.PASSWORD)
+time.sleep(5)
+
+# Wait for connection
+max_attempts = 10
+attempt = 0
+print("attempting connection . . . ")
+while not wlan.isconnected() and attempt < max_attempts:
+    print(f"Trying to connect to env.SSID (Attempt {attempt + 1}/{max_attempts})...")
+    time.sleep(10)
+    attempt += 1
+
+if wlan.isconnected():
+    print("Connected to IP:", wlan.ifconfig()[0])
+else:
+    print("Failed to connect to Wi-Fi. Please check your SSID and password.")
+
+
+app = Microdot()
+CORS(app, allowed_origins = '*', allow_credentials = True)
+
+def angle2duty(angle):
+    K = (7864 - 1802) / (180)
+    return int(angle * K) + 1802
+    
+
+@app.route('/hello_world', methods=['GET'])
+async def get_invoices(request):
+    return 'Hello World!'
+
+@app.get('/')
+@with_websocket
+async def index(request, ws):
+    try: 
+        while True:
+            data = await ws.receive()
+            if not data:
+                break
+            await ws.send('hello world')
+            
+    except OSError as msg:
+        print(msg)
+
+# @app.get("/servo")
+# @with_websocket
+# async def index(request, ws):
+#     while True:
+#         data = await ws.receive()
+#         data = ujson.loads(data)
+#         val = data["servo"]
+#         servo.duty_u16(angle2duty(val))
+
+app.run(port=80)
+
