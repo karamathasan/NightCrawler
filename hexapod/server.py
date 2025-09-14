4b28951710c6596ed659e3badb4f0ac66530fe91
karamathasan <94758689+karamathasan@users.noreply.github.com>
2025-09-14 19:04:03 -0400

    zmq on robot

--- hexapod/server.py
+++ hexapod/server.py
@@ -1,4 +1,5 @@
 import zmq
+import asyncio
 
 import json
 import cv2
@@ -6,8 +7,13 @@ from PIL import Image
 import time
 
 ctx = zmq.Context()
-videoSock = ctx.socket(zmq.PUB)
-videoSock.bind("tcp://*:5555")
+readyRep = ctx.socket(zmq.REP)
+processReq = ctx.socket(zmq.REQ)
+videoPub = ctx.socket(zmq.PUB)
+
+print("Starting 0MQ Server...")
+readyRep.bind("tcp://*:5556")
+videoPub.bind("tcp://*:5555")
 
 cap = cv2.VideoCapture(1)
 
@@ -15,10 +21,22 @@ def getFrame():
     success, frame = cap.read()
     if not success:
         return None
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     _, buffer = cv2.imencode(".jpg",frame)
     return buffer.tobytes()
 
-print("Starting 0MQ Server")
-while True:
-    videoSock.send(getFrame())
-    time.sleep(0.01)
+print("Waiting for client")
+readyRep.recv()
+print("client found!")
+readyRep.send(b"READY")
+time.sleep(0.6)
+
+running = True
+while running:
+    videoPub.send(getFrame())
+    try:
+        readyRep.recv(flags=zmq.NOBLOCK)
+        running = False
+    except:
+        pass
+    time.sleep(0.2)
